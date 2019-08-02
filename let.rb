e552e82c2ac7276c75e4f8983a7fe5feeae38cc1
URA Takefumi <ura.takefumi@gmail.com>
2019-08-03 00:25:36 +0900

    [feat] implement let_to_params_args_body

--- let.rb
+++ let.rb
@@ -1,4 +1,4 @@
-# -*- compile-command: "rake test"; -*-
+# -*- compile-command: "rake test TEST=test/test_let.rb"; -*-
 # frozen_string_literal: true
 
 $LOAD_PATH << __dir__
@@ -12,5 +12,16 @@ module Let
 
   def let?(exp)
     :let == car(exp)
+  end
+
+  def let_to_params_args_body(exp)
+    return nil if !let?(exp)
+
+    bind_exp = car(cdr(exp))
+    [
+      bind_exp.map{|x| car(x)},
+      bind_exp.map{|x| car(cdr(x))},
+      car(cdr(cdr(exp)))
+    ]
   end
 end
--- test/test_let.rb
+++ test/test_let.rb
@@ -9,4 +9,13 @@ class TestLet < Test::Unit::TestCase
     assert_false Let.let?([])
     assert_false Let.let?([:hello])
   end
+
+  def test_let_to_params_args_body
+    params, args, body = Let.let_to_params_args_body([:let, [[:x, 3], [:y, 2]], [:+, :x, :y]])
+    assert_equal [:x, :y], params
+    assert_equal [3, 2], args
+    assert_equal [:+, :x, :y], body
+
+    assert_nil Let.let_to_params_args_body([:hage])
+  end
 end
